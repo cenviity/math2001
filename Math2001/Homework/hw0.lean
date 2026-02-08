4a5df016826bc4f9e8dfc12e36320ea6e724abcf
Vincent Ging Ho Yim <cenviity@gmail.com>
2026-02-08 16:42:07 +1100

    homework: remove `AutograderLib` imports to work around build errors

--- Math2001/Homework/hw0.lean
+++ Math2001/Homework/hw0.lean
@@ -1,7 +1,6 @@
 /- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
 import Mathlib.Tactic.GCongr
 import Library.Basic
-import AutograderLib
 
 math2001_init
 
@@ -11,6 +10,5 @@ Don't forget to compare with the text version
 for clearer statements and any special instructions. -/
 
 
-@[autograded 5]
 theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
   sorry
