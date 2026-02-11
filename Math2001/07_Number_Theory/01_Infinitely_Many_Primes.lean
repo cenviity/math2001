6aa4f31ba51726d788c7f11e1bff3dd20e28b0cf
Vincent Ging Ho Yim <cenviity@gmail.com>
2026-02-12 00:22:03 +1100

    7.1: refactor and simplify example

--- Math2001/07_Number_Theory/01_Infinitely_Many_Primes.lean
+++ Math2001/07_Number_Theory/01_Infinitely_Many_Primes.lean
@@ -7,20 +7,17 @@ namespace Nat
 
 
 example (N : ℕ) : ∃ p ≥ N, Prime p := by
-  have hN0 : 0 < N ! := by apply factorial_pos
-  have hN2 : 2 ≤ N ! + 1 := by addarith [hN0]
+  have hN2 : 2 ≤ N ! + 1 := by addarith [factorial_pos N]
   -- `N! + 1` has a prime factor, `p`
   obtain ⟨p, hp, hpN⟩ : ∃ p : ℕ, Prime p ∧ p ∣ N ! + 1 := exists_prime_factor hN2
-  have hp2 : 2 ≤ p
-  · obtain ⟨hp', hp''⟩ := hp
-    apply hp'
+  have hp2 : 2 ≤ p := hp.1
   obtain ⟨k, hk⟩ := hpN
   match k with
   | 0 => -- if `k` is zero, contradiction
     have k_contra :=
-    calc 0 < N ! + 1 := by extra
-      _ = p * 0 := hk
-      _ = 0 := by ring
+      calc 0 < N ! + 1 := by extra
+        _ = p * 0 := hk
+        _ = 0 := by ring
     numbers at k_contra
   | l + 1 => -- so `k = l + 1` for some `l`
     -- the key fact: `p` is not a factor of `N!`
@@ -29,20 +26,17 @@ example (N : ℕ) : ∃ p ≥ N, Prime p := by
       use l
       constructor
       · have :=
-        calc p * l + p = p * (l + 1) := by ring
-          _ = N ! + 1 := by rw [hk]
-          _ < N ! + p := by addarith [hp2]
+          calc p * l + p = p * (l + 1) := by ring
+            _ = N ! + 1 := by rw [hk]
+            _ < N ! + p := by addarith [hp2]
         addarith [this]
       · calc N ! < N ! + 1 := by extra
           _ = p * (l + 1) := by rw [hk]
     -- so `p` is a prime number greater than or equal to `N`, as we sought
     use p
     constructor
-    · obtain h_le | h_gt : p ≤ N ∨ N < p := le_or_lt p N
-      · have : p ∣ (N !)
-        · apply dvd_factorial
-          · extra
-          · addarith [h_le]
+    · obtain h_le | h_gt : p ≤ N ∨ p > N := le_or_gt p N
+      · have : p ∣ (N !) := dvd_factorial (by extra) h_le
         contradiction
       · addarith [h_gt]
     · apply hp
