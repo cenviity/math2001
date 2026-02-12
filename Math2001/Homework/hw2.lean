655d030bcc28d8fe4484076ea86b55ac62b1ace2
Vincent Ging Ho Yim <cenviity@gmail.com>
2026-02-12 19:27:31 +1100

    hw2: solve exercises

--- Math2001/Homework/hw2.lean
+++ Math2001/Homework/hw2.lean
@@ -12,17 +12,80 @@ for clearer statements and any special instructions. -/
 
 
 theorem problem1 {x : ℚ} (h1 : x ^ 2 = 9) (h2 : 1 < x) : x = 3 := by
-  sorry
+  have :=
+    calc x * (x + 3)
+      _ = x ^ 2 + 3 * x := by ring
+      _ = 9 + 3 * x := by rw [h1]
+      _ = 3 * (x + 3) := by ring
+  cancel x + 3 at this
 
 theorem problem2 {s : ℚ} (h1 : 3 * s ≤ -15) (h2 : 2 * s ≥ -10) : s = -5 := by
-  sorry
+  have h1' :=
+    calc
+      s = 3 * s / 3 := by ring
+      _ ≤ -15 / 3 := by rel [h1]
+      _ = -5 := by numbers
+  have h2' :=
+    calc
+      s = 2 * s / 2 := by ring
+      _ ≥ -10 / 2 := by rel [h2]
+      _ = -5 := by numbers
+  apply le_antisymm h1' h2'
 
 theorem problem3 {t : ℚ} (h : t = 2 ∨ t = -3) : t ^ 2 + t - 6 = 0 := by
-  sorry
+  obtain h | h := h
+  · rw [h]
+    numbers
+  · rw [h]
+    numbers
 
 theorem problem4 {x : ℤ} : 3 * x ≠ 10 := by
-  sorry
+  obtain hx | hx : x ≤ 3 ∨ 4 ≤ x := le_or_succ_le x 3
+  · apply ne_of_lt
+    calc 3 * x
+      _ ≤ 3 * 3 := by rel [hx]
+      _ < 10 := by numbers
+  · apply ne_of_gt
+    calc 3 * x
+      _ ≥ 3 * 4 := by rel [hx]
+      _ > 10 := by numbers
 
 theorem problem5 {x y : ℝ} (h1 : 2 ≤ x ∨ 2 ≤ y) (h2 : x ^ 2 + y ^ 2 = 4) :
     x ^ 2 * y ^ 2 = 0 := by
-  sorry
+  obtain hx | hy := h1
+  · have hx1 :=
+      calc x ^ 2
+        _ = x ^ 2 + y ^ 2 - y ^ 2 := by ring
+        _ = 4 - y ^ 2 := by rw [h2]
+        _ ≤ 4 - y ^ 2 + y ^ 2 := by extra
+        _ = 4 := by ring
+    have hx2 :=
+      calc x ^ 2
+        _ ≥ 2 ^ 2 := by rel [hx]
+        _ = 4 := by numbers
+    have hx3 : x ^ 2 = 4 := le_antisymm hx1 hx2
+    have hy :=
+      calc y ^ 2
+        _ = x ^ 2 + y ^ 2 - x ^ 2 := by ring
+        _ = 4 - 4 := by rw [h2, hx3]
+        _ = 0 := by numbers
+    rw [hx3, hy]
+    numbers
+  · have hy1 :=
+      calc y ^ 2
+        _ = x ^ 2 + y ^ 2 - x ^ 2 := by ring
+        _ = 4 - x ^ 2 := by rw [h2]
+        _ ≤ 4 - x ^ 2 + x ^ 2 := by extra
+        _ = 4 := by ring
+    have hy2 :=
+      calc y ^ 2
+        _ ≥ 2 ^ 2 := by rel [hy]
+        _ = 4 := by numbers
+    have hy3 : y ^ 2 = 4 := le_antisymm hy1 hy2
+    have hx :=
+      calc x ^ 2
+        _ = x ^ 2 + y ^ 2 - y ^ 2 := by ring
+        _ = 4 - 4 := by rw [h2, hy3]
+        _ = 0 := by numbers
+    rw [hx, hy3]
+    numbers
